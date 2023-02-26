5359c00466b3986c917a2d21f5338ce72af2cce6
Tim Tanatarov <112192293+timtanatarov@users.noreply.github.com>
2023-02-26 12:58:10 +0300

    Delete task2.js

--- Tanatarov-Timur-Lesson-1/task2.js
+++
@@ -1,6 +0,0 @@
-const getNumberRadix = (number, radix) => {
-    if (+number % 1 === 0 && (radix > 1 && radix < 17)) {
-        return (+number).toString(radix);
-    }
-    throw Error ('Функция getNumberRadix была вызвана с некорректными параметрами');
-};
