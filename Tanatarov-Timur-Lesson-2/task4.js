d83eed7db446cb044a7cb4dc8969f827ba3dc68c
Tim Tanatarov <112192293+timtanatarov@users.noreply.github.com>
2023-03-06 20:19:23 +0300

    Delete task4.js

--- Tanatarov-Timur-Lesson-2/task4.js
+++
@@ -1,8 +0,0 @@
-const getUniqArray = arr => {
-    arr.forEach(value => {
-        if (typeof value !== 'number') {
-            throw Error('В getUniqArray был передан невалидный параметр. Аргумент arr должен быть массивом чисел.');
-        }
-    });
-    return Array.from(new Set(arr));
-};
